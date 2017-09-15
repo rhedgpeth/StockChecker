c6d47216a06996d74a6edf44e3df0321949f1074
Robert Hedgpeth <robert.hedgpeth@xamarin.com>
2017-09-15 16:39:18 -0500

    Added Mobile Center integrations

--- StockChecker/App.xaml.cs
+++ StockChecker/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Xamarin.Forms;
 
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
 namespace StockChecker
 {
     public partial class App : Application
@@ -13,7 +17,9 @@ namespace StockChecker
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+			MobileCenter.Start("ios=31834509-ddc5-4030-ac13-7049a57a57b8;" +
+				   //"uwp={Your UWP App secret here};",
+				   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
